06cc7a1cf73317a5656536b50114772c42757bfb
RayenNasri <rayennasr679@gmail.com>
2025-03-28 02:12:40 +0100

    up

--- front/NS/vite.config.ts
+++ front/NS/vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  // Base public path when served in production
-  base: mode === 'production' ? './' : '/',
+  base: '/',  
   
-  // Build configuration
   build: {
     outDir: 'dist',
     minify: 'terser',
@@ -24,6 +21,5 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     tailwindcss(),
-
   ],
 }))
